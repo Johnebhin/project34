058f8eda39863c7b1077e337064bfba34556dbdd
Johnebhin <121502677+Johnebhin@users.noreply.github.com>
2023-04-06 19:08:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -54,9 +54,9 @@ function blow() {
   // Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0, y: -0.05 });
   
   // Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0.05, y: 0.05 });
-  Matter.Body.applyForce(ball.body,{x:0,y:0}<{x:0,y:0.05});
+  //Matter.Body.applyForce(ball.body,{x:0,y:0}<{x:0,y:0.05});
   
-  //Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0, y: 0.05 });
+  Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0, y: 0.05 });
   
   // Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: -0.05, y: 0.05 });
 
